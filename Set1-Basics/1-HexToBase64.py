801bea136fa9780f788d835e21adb03a604effb8
Karsten <Karsten.Huttelmaier@gmail.com>
2022-04-09 09:21:18 -0700

    Misc and project update

--- Set1-Basics/1-HexToBase64.py
+++ Set1-Basics/1-HexToBase64.py
@@ -11,4 +11,6 @@ def hex_to_base_64(value):
 
 hex_input = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
 expected = b'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
-print(hex_to_base_64(hex_input) == expected)
+
+print(hex_to_base_64(hex_input))
+assert(hex_to_base_64(hex_input) == expected)
